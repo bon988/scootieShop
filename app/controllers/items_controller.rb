d60e50a8092cb39f94e7e82d2b9d262fae9df49f
Ubuntu <ubuntu@ip-172-31-47-26.ec2.internal>
2020-04-10 22:38:50 +0000

    CSS and HTML updates to edit profile page plus other pages and partial categories

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,7 +17,7 @@ class ItemsController < ApplicationController
      st = "%#{params[:q]}%"
      @items = Item.where("category like ?", st)
   end
-
+  
   # GET /items/new
   def new
     @item = Item.new
